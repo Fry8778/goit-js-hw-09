5883103897d1de408f39c6e6543465792216f920
Fry8778 <102724727+Fry8778@users.noreply.github.com>
2022-07-04 21:15:33 +0000

    Deploying to gh-pages from @ Fry8778/goit-js-hw-09@2f8de45d29d90cc30bc74dbb3ce71e0acfd9e154 🚀

---
+++ 01-color-switcher.3424a7d9.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector("[data-start]");document.querySelector("[data-stop]").addEventListener("click",(function(){}))}();
+//# sourceMappingURL=01-color-switcher.3424a7d9.js.map
---
+++ 01-color-switcher.34f92791.js
@@ -0,0 +1,2 @@
+document.querySelector("[data-start]");document.querySelector("[data-stop]").addEventListener("click",(()=>{}));
+//# sourceMappingURL=01-color-switcher.34f92791.js.map
