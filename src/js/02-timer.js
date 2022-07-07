ff4c04b38042c89dbd9961f473a033b7fe767285
Vitaliy Kulinich <vitaliy.kulinich88@gmail.com>
2022-07-07 16:24:38 +0300

    Corrected 2 task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,13 +2,14 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const btnStr = document.querySelector('button[data-start]');
+const btnStr = document.querySelector('[data-start]');
 const daysEl = document.querySelector('[data-days]');
 const hoursEl = document.querySelector('[data-hours]');
 const minutesEl = document.querySelector('[data-minutes]');
 const secondsEl = document.querySelector('[data-seconds]');
 const inputEl = document.querySelector('#datetime-picker');
 
+btnStr.disabled = true;
 flatpickr('#datetime-picker', {
     enableTime: true,
     time_24hr: true,
@@ -16,48 +17,51 @@ flatpickr('#datetime-picker', {
     minuteIncrement: 1,
     onClose(selectedDates) {
         console.log(selectedDates[0]);
-        const difference = selectedDates[0] - Date.now();
-        if (difference < 1000) {
+        selected = selectedDates[0].getTime();        
+        if (selected < new Date()) {
             Notify.failure('Please choose a date in the future');
+            btnStr.disabled = true;
             return;
         }
         btnStr.disabled = false;
-        const interval = setInterval(() => {
-            btnStr.disabled = true;
-            inputEl.disabled = true;
-            const difference = selectedDates[0] - Date.now();
-            if (difference < 1000) {
-                btnStr.disabled = false;
-                inputEl.disabled = false;
-                stop();
-            };
-            const { days, hours, minutes, seconds } = convertMs(difference);
-            daysEl.textContent = pad(days);
-            hoursEl.textContent = pad(hours);
-            minutesEl.textContent = pad(minutes);
-            secondsEl.textContent = pad(seconds);
-        }, 1000);
-        function stop() {
-            clearInterval(interval);
-        }
-
-        function pad(num) {
-            return num.toString().padStart(2, '0');
-        }
-
-
+        
+        
     },
 });
+
+function pad(num) {
+    return num.toString().padStart(2, '0');
+}
+
+const startBtn = () => {
+    const interval = setInterval(() => {
+        btnStr.disabled = true;
+        inputEl.disabled = true;
+        const difference = selected - Date.now();
+        if (difference < 1000) {
+            btnStr.disabled = false;
+            inputEl.disabled = false;
+            clearInterval(interval);
+        };
+        const { days, hours, minutes, seconds } = convertMs(difference);
+        daysEl.textContent = pad(days);
+        hoursEl.textContent = pad(hours);
+        minutesEl.textContent = pad(minutes);
+        secondsEl.textContent = pad(seconds);
+    }, 1000);
+}
 
 function convertMs(ms) {
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
-
+    
     const days = Math.floor(ms / day);
     const hours = Math.floor((ms % day) / hour);
     const minutes = Math.floor(((ms % day) % hour) / minute);
     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
     return { days, hours, minutes, seconds };
 };
+
+btnStr.addEventListener ('click', startBtn);
